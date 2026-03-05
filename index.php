755a8b8a8dba05c03b81efd95d3aad78dbb3b8c3
hnoor1 <hnoor@hkhk.edu.ee>
2026-03-05 12:22:34 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php include('config.php');?>
 
 <!doctype html>
@@ -88,4 +89,96 @@
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
+=======
+<?php include('config.php');?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+
+  <body>
+  <!-- menüü!, BS kopeeritud navbar  -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
+  <div class="container">
+    <a class="navbar-brand" href="#">Autorent</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Link</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search" method="get" action="index.php">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="otsi"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<!-- menüü  -->
+<!-- Sisu, Võtsin Bootstrapist Cards  -->
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+<!-- üks auto  -->
+<?php
+    $paring = "SELECT * FROM cars";                 //valmistan ette päringu stringina 
+    if (!empty($_GET["otsi"])) {
+        $otsing = $_GET["otsi"];
+        $paring .= " WHERE mark LIKE '%".$otsing."%'";
+    } 
+
+    $paring .= " LIMIT 8";
+
+    //var_dump($_GET["otsi"]);
+
+    $valjund = mysqli_query($yhendus, $paring);     //saadan päringu andmebaasi
+    while($rida = mysqli_fetch_assoc($valjund)){    //sikutan vastuse alla, while tsükliga kuvab kaartidesse autosid koos andmetega
+        // var_dump($rida);                            // //kuvan testvastuse
+                                                  
+    
+?>
+
+  <div class="col">
+    <div class="card">
+      <img src="https://loremflickr.com/400/250/<?php echo str_replace(" ","",$rida ["mark"]); ?>" class="card-img-top" alt="<?php echo str_replace(" ","",$rida ["mark"]); ?>">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $rida ["mark"]; ?> <?php echo $rida ["model"]; ?></h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            Mootor: <?php echo $rida ["engine"]; ?> <br>
+            Kütus: <?php echo $rida ["fuel"]; ?> <br>
+            Hind: <?php echo $rida ["price"]; ?> €/päev <br>
+        </p>
+        <a href="single_car.php?id=<?php echo $rida["id"]; ?>" class="btn btn-primary w-100">Rendi</a>
+        </div>
+        </div>
+  </div>
+  <?php } ?>
+        <!-- üks auto  -->
+  
+  </div>
+</div>  
+
+</div>
+<!-- Sisu  -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+>>>>>>> 83f4fcbc891a7586891f38798fc109e1c1d7f1f2
 </html>
